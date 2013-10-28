abc2de393ed7427920630ff90ab25ff3a105e818
Alphadelta14 <alpha@pokesplash.net>
2013-10-27 23:53:53 -0400

    APP: Serve to localhost

--- web/main.py
+++ web/main.py
@@ -8,7 +8,11 @@
 #################################################################### 
 
 from flask import Flask
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__, template_folder='../templates',
     static_folder='../static')
 
+if __name__ == "__main__":
+    http = WSGIServer(('127.0.0.1', 5000), app)
+    http.serve_forever()
