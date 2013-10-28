f3086818eaa03aab3f70917855c794341812ca36
Alphadelta14 <alpha@pokesplash.net>
2013-10-28 02:42:42 -0400

    USERBP: Added signup route

--- web/user.py
+++ web/user.py
@@ -12,6 +12,7 @@ from flask import Blueprint, render_template, request, session
 __all__ = ["user_blueprint"]
 
 INVALID_USER = "Incorrect Username/Password"
+MISSING_FIELDS = "All fields are required"
 
 user_blueprint = Blueprint("user_pages", __name__)
 
@@ -33,3 +34,21 @@ def login():
         # TODO: Database auth
     else:
         return render_template("user/login.html")
+
+@user_blueprint.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        data = request.form
+        if not data:
+            return render_template("user/signup.html", error="Bad request")
+        username = data.get("username", "")
+        email = data.get("email", "")
+        email2 = data.get("email2", "")
+        password = data.get("password", "")
+        password2 = data.get("password2", "")
+        if not (username and email and email2 and password and password2):
+            return render_template("user/signup.html", error=MISSING_FIELDS,
+                data=data)
+        # TODO: Database auth
+    else:
+        return render_template("user/signup.html")
