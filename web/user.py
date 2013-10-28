26819ebdc7934b5f0201c7c0202cb7d243f08869
Alphadelta14 <alpha@pokesplash.net>
2013-10-28 02:09:07 -0400

    USERBP: Add login route

--- web/user.py
+++ web/user.py
@@ -11,9 +11,25 @@ from flask import Blueprint, render_template
 
 __all__ = ["user_blueprint"]
 
+INVALID_USER = "Incorrect Username/Password"
+
 user_blueprint = Blueprint("user_pages", __name__)
 
 @user_blueprint.route("/")
 @user_blueprint.route("/index")
 def index():
     return render_template("user/index.html")
+
+@user_blueprint.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        data = request.json
+        if not data:
+            return render_template("user/login.html", error="Bad request")
+        username = data.get("username", "")
+        password = data.get("password", "")
+        if not username or not password:
+            return render_template("user/login.html", error=INVALID_USER)
+        # TODO: Database auth
+    else:
+        return render_template("user/login.html")
