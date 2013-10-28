af415cbb2d9f49a71a962b657fc33413a2a82034
Alphadelta14 <alpha@pokesplash.net>
2013-10-28 00:49:54 -0400

    USERBP: Added user_blueprint to main routes

--- web/main.py
+++ web/main.py
@@ -11,11 +11,12 @@ from flask import Flask
 from gevent.pywsgi import WSGIServer
 
 from web.home import home_blueprint
+from web.user import user_blueprint
 
 app = Flask(__name__, template_folder='../templates',
     static_folder='../static')
 app.register_blueprint(home_blueprint)
-
+app.register_blueprint(user_blueprint)
 
 if __name__ == "__main__":
     http = WSGIServer(('127.0.0.1', 5000), app)
