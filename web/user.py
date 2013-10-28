e473694a02707d08fcd7e32feddf8bf24d77d66c
Alphadelta14 <alpha@pokesplash.net>
2013-10-28 00:47:33 -0400

    APP: Created user blueprint

---
+++ web/user.py
@@ -0,0 +1,19 @@
+""" @package web.user Flask Web Application: user blueprints """
+####################################################################
+#
+#       Modeling Wars Web Application User routes
+#
+#       Author: Alpha <alpha@pokesplash.net>
+#
+####################################################################
+
+from flask import Blueprint, render_template
+
+__all__ = ["@user_blueprint"]
+
+user_blueprint = Blueprint("user_pages", __name__)
+
+@user_blueprint.route("/")
+@user_blueprint.route("/index")
+def index():
+    return render_template("user/index.html")
