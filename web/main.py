25cb338077a81d0ae0720a914fccef5c87b443a8
Alphadelta14 <alpha@pokesplash.net>
2013-10-28 01:28:56 -0400

    CONFIG: Added apply_config into app

--- web/main.py
+++ web/main.py
@@ -12,11 +12,13 @@ from gevent.pywsgi import WSGIServer
 
 from web.home import home_blueprint
 from web.user import user_blueprint
+from config.web_config import apply_config
 
 app = Flask(__name__, template_folder='../templates',
     static_folder='../static')
 app.register_blueprint(home_blueprint)
 app.register_blueprint(user_blueprint, url_prefix="/user")
+apply_config(app)
 
 if __name__ == "__main__":
     http = WSGIServer(('127.0.0.1', 5000), app)
