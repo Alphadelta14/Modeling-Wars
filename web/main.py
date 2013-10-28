a9a1e2b1f17975d746fd20f2ce7ee9025147139e
Alphadelta14 <alpha@pokesplash.net>
2013-10-28 00:02:44 -0400

    APP: Register home bp

--- web/main.py
+++ web/main.py
@@ -10,8 +10,12 @@
 from flask import Flask
 from gevent.pywsgi import WSGIServer
 
+from web.home import home
+
 app = Flask(__name__, template_folder='../templates',
     static_folder='../static')
+app.register_blueprint(home)
+
 
 if __name__ == "__main__":
     http = WSGIServer(('127.0.0.1', 5000), app)
